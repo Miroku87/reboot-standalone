7e9e4173c6da52c776db532e6bf56f7653508660
Miroku87 <miroku_87@yahoo.it>
2019-02-10 23:23:29 +0100

    creata griglia di grafici

---
+++ monitoring_interface/app/config.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Miroku on 13/10/2018.
+ */
+
+window.CONFIG = {
+    players_init_energy_percentage : 0,
+    buttons: [
+        {
+            text: "Turboascensori palazzi residenziali",
+            state_percentage_offset: {
+                on: -20,
+                idle: 10,
+                off: 0
+            },
+            can_switch_off : true,
+            init_state: "off"
+        },
+        {
+            text: "Griglia di intercomunicazione ad ampio raggio",
+            state_percentage_offset: {
+                on: -8,
+                idle: 0,
+                off: 0
+            },
+            can_switch_off : false,
+            init_state: "off"
+        },
+        {
+            text: "Sistema di illuminazione stradale d’emergenza",
+            state_percentage_offset: {
+                on: -10,
+                idle: 5,
+                off: 0
+            },
+            can_switch_off : true,
+            init_state: "off"
+        },
+        {
+            text: "Alimentazione Impianto idrico",
+            state_percentage_offset: {
+                on: -5,
+                idle: 0,
+                off: 0
+            },
+            can_switch_off : false,
+            init_state: "off"
+        },
+        {
+            text: "Sistema difensivo automatico – Sezione Nord",
+            state_percentage_offset: {
+                on: -50,
+                idle: 20,
+                off: 0
+            },
+            can_switch_off : false,
+            offline_after_time: 36000, //secondi
+            init_state: "off"
+        },
+        {
+            text: "Accesso alla rete locale Ravnet",
+            state_percentage_offset: {
+                on: -20,
+                idle: 0,
+                off: 0
+            },
+            can_switch_off : false,
+            init_state: "off"
+        },
+        {
+            text: "Reticolo elettrificato",
+            state_percentage_offset: {
+                on: 0,
+                idle: 0,
+                off: 0
+            },
+            can_switch_off : false,
+            init_state: "offline"
+        },
+        {
+            text: "Sistema direzionale Synapse",
+            state_percentage_offset: {
+                on: -5,
+                idle: 0,
+                off: 0
+            },
+            can_switch_off : false,
+            init_state: "off"
+        },
+        {
+            text: "Impianto controllo Proxy",
+            state_percentage_offset: {
+                on: -30,
+                idle: 10,
+                off: 0
+            },
+            can_switch_off : true,
+            init_state: "off"
+        }
+    ]
+};
---
+++ monitoring_interface/app/scripts/manager.js
@@ -0,0 +1,105 @@
+/**
+ * Created by Miroku on 11/10/2018.
+ */
+
+var Manager = Manager || function ()
+    {
+        var KEYS = {
+            CTRL  : 17,
+            SHIFT : 16,
+            A     : 65
+        };
+
+        return {
+
+            bringToBackground : function (ev)
+            {
+                var t = $(ev.currentTarget);
+
+                if (!t.hasClass("active"))
+                    t.removeClass("foreground");
+            },
+
+            scaleChart : function scaleChart(ev)
+            {
+                var t = $(ev.currentTarget).parent();
+                if (!t.hasClass("active"))
+                {
+                    t.addClass("active");
+                    t.addClass("foreground");
+                }
+                else if (t.hasClass("active"))
+                {
+                    t[0].removeEventListener("transitionend", this.bringToBackground);
+                    t[0].addEventListener("transitionend", this.bringToBackground);
+
+                    t.removeClass("active");
+                }
+            },
+
+            resetAll : function resetAll()
+            {
+                window.localStorage.clear();
+                window.location.reload();
+            },
+
+            closeAdminPanel : function closeAdminPanel(e)
+            {
+                this.admin_panel.hide();
+            },
+
+            openAdminPanel : function openAdminPanel(e)
+            {
+                $("#admin_panel").find("tr").not($("#admin_panel").find("tr").first()).remove();
+                this.admin_panel.show();
+            },
+
+            checkShortCuts : function checkShortCuts(e)
+            {
+                if (!this.keys)
+                    this.keys = {};
+
+                this.keys[e.which] = true;
+
+                if (this.keys[KEYS.CTRL] && this.keys[KEYS.SHIFT] && this.keys[KEYS.A])
+                    this.openAdminPanel();
+            },
+
+            onKeyUp : function onKeyUp(e)
+            {
+                if (this.keys && this.keys[e.which])
+                    delete this.keys[e.which];
+            },
+
+            setVideoPlayback : function setListeners()
+            {
+                this.videos.each(function ()
+                {
+                    this.playbackRate = 0.5;
+                });
+            },
+
+            setListeners : function setListeners()
+            {
+                $(document).keydown(this.checkShortCuts.bind(this));
+                $(document).keyup(this.onKeyUp.bind(this));
+                $("#admin_close").click(this.closeAdminPanel.bind(this));
+                this.videos.click(this.scaleChart.bind(this))
+            },
+
+            getDOMElements : function getDOMElements()
+            {
+                this.admin_panel = $("#admin_background");
+                this.videos      = $("video");
+            },
+
+            init : function init()
+            {
+                this.getDOMElements();
+                this.setListeners();
+                this.setVideoPlayback();
+            }
+        };
+    }();
+
+$(document).ready(Manager.init.bind(Manager));
